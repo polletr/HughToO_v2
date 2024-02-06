9f284482bc5d2aef7c801573b33c9da6df850189
Mos <musse.hidru@outlook.com>
2024-02-06 12:59:27 -0700

    PlayerControllerStart
    collected ref and making new structure

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu : MonoBehaviour
+{
+
+    [Header("Menu Game Objects")]
+    [SerializeField]
+    private GameObject _mainMenu;
+    [SerializeField]
+    private GameObject _creditsMenu;
+    [SerializeField]
+    private GameObject _optionsMenu;
+    [SerializeField]
+    private GameObject _controlsMenu;
+
+
+}
