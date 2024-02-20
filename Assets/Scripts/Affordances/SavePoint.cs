9985eb800e13a028f9c456456f7ef083f29abf57
Mos <musse.hidru@outlook.com>
2024-02-20 15:18:25 -0700

    Check points , Save points

---
+++ Assets/Scripts/Affordances/SavePoint.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+[RequireComponent(typeof(Collider2D))]
+public class SavePoint : MonoBehaviour
+{
+    [SerializeField] private AudioClip _teleportOutClip, _teleportInClip;
+    [SerializeField] private GameObject _teleportParticlePrefab;
+    [SerializeField] private float _teleportDelay = 0.5f;
+
+    [SerializeField] private PlayerBaseInfo PlayerData;
+    [SerializeField] private GameObject _blackScreen;
+
+    private Vector3 _teleportPosition;
+
+    Player player;
+
+    private void Awake()
+    {
+        GetComponent<Collider2D>().isTrigger = true;
+    }
+    [SerializeField]
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            player = other.GetComponent<Player>();
+            player.playerData.Data.position[0] = transform.position.x;
+            player.playerData.Data.position[1] = transform.position.y;
+            player.playerData.Data.position[2] = transform.position.z;
+
+            SaveManager.Instance.SavePlayerData();
+        }
+    }
+
+}
--- Assets/Scripts/Affordances/Water.cs
+++ Assets/Scripts/Affordances/Water.cs
@@ -7,16 +7,20 @@ public class Water : MonoBehaviour
 {
 
     public UnityEvent playerDrowned;
+    public UnityEvent<Vector3> SpawnPos;
+
 
     [SerializeField]
     private int damage;
-
+    public Vector3 _teleportPosition;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             playerDrowned?.Invoke();
+            SpawnPos?.Invoke(_teleportPosition);
             collision.GetComponent<PlayerHealth>().TakeDamage(damage);
+
         }
     }
 }
