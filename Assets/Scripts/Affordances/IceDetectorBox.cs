c3084dc84c12345365b4b1484195207b4c714bff
Mos <musse.hidru@outlook.com>
2024-02-20 15:57:00 -0700

    Hint Box / Water Ice Affordance

---
+++ Assets/Scripts/Affordances/IceDetectorBox.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[RequireComponent(typeof(BoxCollider2D))]
+public class IceDetectorBox : MonoBehaviour
+{
+
+        [SerializeField]
+        GameObject _iceBox;
+
+
+        void Start()
+        {
+            GetComponent<BoxCollider2D>().isTrigger = true;
+            if (_iceBox != null)
+                _iceBox.SetActive(false);
+        }
+
+        private void OnTriggerStay2D(Collider2D other)
+        {
+            if (other.GetComponent<Player>().currentStats.currentForm == ScriptableStats.Form.Ice)
+            {
+                if (_iceBox != null)
+                    _iceBox.SetActive(true);
+            }
+        }
+
+        private void OnTriggerExit2D(Collider2D other)
+        {
+            if (other.CompareTag("Player"))
+            {
+                if (_iceBox != null)
+                    _iceBox.SetActive(false);
+            }
+        }
+
+    }
