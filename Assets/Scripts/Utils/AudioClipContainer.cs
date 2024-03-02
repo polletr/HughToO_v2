eb19be73dee52fa770d1381b0c9d3c2f77999745
Musse <musse.hidru@outlook.com>
2024-03-02 07:36:35 -0700

    Audio container

--- Assets/Scripts/Utils/AudioClipContainer.cs
+++ Assets/Scripts/Utils/AudioClipContainer.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu]
 public class AudioClipContainer : ScriptableObject
@@ -7,10 +5,30 @@ public class AudioClipContainer : ScriptableObject
     [Header("BGMusic")]
 
     public AudioClip MainMenu;
-    public AudioClip SummerPlace;
-    public AudioClip WinterPlace;
-    public AudioClip SpringPlace;
-    public AudioClip AutumnPlace;
+    public AudioClip Cave;
+    public AudioClip DeepCave;
+    public AudioClip Grassland;
+
+    [Header("WorldSFX")]
+
+    public AudioClip CollectedPickup;
+    public AudioClip TeleportOut;
+    public AudioClip TeleportIn;
+    public AudioClip Teleport;
+
+
+    [Header("PlayerSFX")]
+
+    public AudioClip Jump;
+    public AudioClip Death;
+    public AudioClip Glide;
+    public AudioClip Drown;
+    public AudioClip Hit;
+
+    public AudioClip WaterSplash;
+    public AudioClip IcePunch;
+    public AudioClip WindStun;
+
 
 
 }
