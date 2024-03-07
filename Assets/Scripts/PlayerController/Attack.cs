e3bc7000de8ba68e4ee48ff02ed05caf12d8c688
Renee Camillo Pollet <r.camillopollet798@mybvc.ca>
2024-03-06 17:35:34 -0700

    MushElevator Fix

--- Assets/Scripts/PlayerController/Attack.cs
+++ Assets/Scripts/PlayerController/Attack.cs
@@ -24,58 +24,59 @@ public class Attack : MonoBehaviour
     void Check(ScriptableStats stats, GameObject interactableObj)
     {
         InteractableObjects.ObjectType objType = interactableObj.GetComponent<InteractableObjects>().currentType;
+        
         switch (objType)
         {
             case InteractableObjects.ObjectType.Fire:
                 if (stats.currentForm == ScriptableStats.Form.Water)//Check if the State is Water
                 {
-                    interactableObj.GetComponent<InteractableObjects>().KillObj();
+                    interactableObj?.GetComponent<InteractableObjects>().KillObj();
                 }
                 else if (stats.currentForm == ScriptableStats.Form.Ice)
                 {
-                    interactableObj.GetComponent<HurtCollider>().DoDamage(stats, player.GetComponent<PlayerHealth>()) ;
+                    interactableObj?.GetComponent<HurtCollider>().DoDamage(stats, player.GetComponent<PlayerHealth>()) ;
                     player.ChangeState(new KnockBackState());
                 }
                 else if (stats.currentForm == ScriptableStats.Form.Gas)
                 {
-                    interactableObj.GetComponent<InteractableObjects>().InteractObj();
+                    interactableObj?.GetComponent<InteractableObjects>().InteractObj();
                 }
                 break;
             case InteractableObjects.ObjectType.BreakableWall:
                 if (stats.currentForm == ScriptableStats.Form.Ice) //Check if the State is Ice
                 {
-                    interactableObj.GetComponent<InteractableObjects>().KillObj();
+                    interactableObj?.GetComponent<InteractableObjects>().KillObj();
                 }
                 break;
             case InteractableObjects.ObjectType.Enemy:
                 if (stats.currentForm == ScriptableStats.Form.Gas)
                 {
-                    interactableObj.GetComponent<EnemyBase>().Stun();
+                    interactableObj?.GetComponent<EnemyBase>().Stun();
                 }
                 else if (stats.currentForm == ScriptableStats.Form.Ice)
                 {
-                    interactableObj.GetComponent<EnemyBase>().TakeDamage(stats.damage);
+                    interactableObj?.GetComponent<EnemyBase>().TakeDamage(stats.damage);
                 }
                 break;
             case InteractableObjects.ObjectType.Mushroom:
-                if (stats.currentForm == ScriptableStats.Form.Water)
+                if (stats?.currentForm == ScriptableStats.Form.Water)
                 {
-                    interactableObj.GetComponent<GrowingTree>().OnInteracted();
-                    interactableObj.GetComponent<InteractableObjects>().InteractObj();
+                    interactableObj?.GetComponent<GrowingTree>().OnInteracted();
+                    interactableObj?.GetComponent<InteractableObjects>().InteractObj();
                 }
                 break;
             case InteractableObjects.ObjectType.Vine:
                 if (stats.currentForm == ScriptableStats.Form.Water)
                 {
-                    interactableObj.GetComponent<GrowingVines>().OnInteracted();
-                    interactableObj.GetComponent<InteractableObjects>().InteractObj();
+                    interactableObj?.GetComponent<GrowingVines>().OnInteracted();
+                    interactableObj?.GetComponent<InteractableObjects>().InteractObj();
 
                 }
                 break;
             case InteractableObjects.ObjectType.Bullet:
                 if (stats.currentForm == ScriptableStats.Form.Gas)
                 {
-                    interactableObj.GetComponent<InteractableObjects>().InteractObj();
+                    interactableObj?.GetComponent<InteractableObjects>().InteractObj();
 
                 }
                 break;
